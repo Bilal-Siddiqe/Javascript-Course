241a9aca026e7f45e32db79a103b6efbcf4f4371
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2023-01-06 12:46:06 +0500

    use strict added with var

---
+++ Javascript Beigners/Variables.js
@@ -0,0 +1,22 @@
+"use strict";
+// intro to variables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable 
+
+var firstName = "Bilal";
+
+// use a variable 
+console.log(firstName);
+
+
+
+// change value 
+var firstName = "Bilal";
+
+console.log(firstName);
+
+
