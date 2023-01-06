eb10951b612feca775661ff6270017f9fcd8b099
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2023-01-06 13:34:40 +0500

    Slice method with practice

---
+++ Javascript Beigners/04_Slice_string.js
@@ -0,0 +1,17 @@
+let name = "Shahzaib";
+
+name.slice(1,3);
+
+console.log(name);
+
+
+
+
+let name_aray = ["Bilal","Furqan", "Rizwan", "Ali"];
+
+name_aray.slice(1,3);
+
+console.log(name_aray);
+
+
+
