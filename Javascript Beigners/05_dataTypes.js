18d9cacd439e4c49ac00055fcafa4b207b673aab
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2023-01-15 17:25:59 +0500

    Data types  tried

---
+++ Javascript Beigners/05_dataTypes.js
@@ -0,0 +1,29 @@
+
+
+// let obj = {name :"Bilal"};
+
+// console.log(name)
+
+// document.write(typeof(obj))
+
+
+
+function chk(){
+
+let a = document.getElementById("user").value;
+
+
+// let a = null;
+
+
+if(a === 2 ){
+   console.log("equal")
+}
+else{
+    console.log("not ")
+}
+}
+
+
+
+
